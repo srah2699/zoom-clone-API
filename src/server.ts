846aa5acdf9394af1db5c13d9d007a99c1144571
Raghavendra Eeti <raghavendra.eeti@Raghavendras-MacBook-Pro.local>
2021-09-14 19:31:52 +0530

    Made main function async

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { Application } from "express";
 
+import configs from "./configs";
 import routes from "./routes";
 
 const PORT = process.env.PORT || 5000;
 const app: Application = express();
 
-function main () {
+async function main () {
+
+    await configs.database();
 
     app.use("/users", routes.users);
     app.use("/meetings", routes.meetings);
